7e475166b6249e6cfc3d827199874a000aa5f2bb
Tal Mogendorff <71597455+TalMogendorff@users.noreply.github.com>
2023-07-19 18:04:49 +0300

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "63a07cd2-3066-4be4-9a16-dd8e39755599" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001);
